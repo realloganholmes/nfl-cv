8b52eddf399080c57087b1878adc9316a20a6c8b
realloganholmes <logan.holmes@utah.edu>
2026-04-03 13:45:18 -0400

    Instant update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -229,7 +229,7 @@ export default function App() {
 
   useEffect(() => {
     onTimeUpdate();
-  }, [showPlayers, results]);
+  }, [showPlayers, results, visibleTrackIds]);
 
   useEffect(() => {
     if (fieldImageReady) {
